6ee2a19bf6af7ba5a7519d4c42789c639951a2dd
Ankita Pabbi <erankitapabbi@gmail.com>
2019-05-23 19:06:50 -0400

    [Ankita] i added my name 20 times

--- src/com/lambton/CollegeSystem.java
+++ src/com/lambton/CollegeSystem.java
@@ -4,7 +4,7 @@ public class CollegeSystem {
     public static void main(String[] args) {
         int i;
        // System.out.println("My name is ankita");
-        System.out.println("Hello World");
+        //System.out.println("Hello World");
         for(i = 0 ;i<10;i++){
             System.out.println("Ankita Pabbi");
         }
