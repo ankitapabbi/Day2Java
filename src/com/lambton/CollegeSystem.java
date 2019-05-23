a5ae2db85567c41a8eda79d6f70a6de7125a1c88
Ankita Pabbi <erankitapabbi@gmail.com>
2019-05-23 19:36:30 -0400

    [Ankita] New Sum logic added

--- src/com/lambton/CollegeSystem.java
+++ src/com/lambton/CollegeSystem.java
@@ -3,10 +3,16 @@ package com.lambton;
 public class CollegeSystem {
     public static void main(String[] args) {
         int i;
+        int sum=0;
        // System.out.println("My name is ankita");
         //System.out.println("Hello World");
-        for(i = 0 ;i<10;i++){
-            System.out.println("Ankita Pabbi");
+//        for(i = 0 ;i<10;i++){
+//            System.out.println("Ankita Pabbi");
+//        }
+        for(i=1;i<=10;i++){
+            sum=sum+1;
         }
+        System.out.println("Sum is : "+sum);
+
     }
 }
