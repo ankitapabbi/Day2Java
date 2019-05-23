68a8e21f3faaee6381d79689d8eeccc746c979ac
Ankita Pabbi <erankitapabbi@gmail.com>
2019-05-23 19:00:05 -0400

    [Ankita] i added my name

--- src/com/lambton/CollegeSystem.java
+++ src/com/lambton/CollegeSystem.java
@@ -2,7 +2,7 @@ package com.lambton;
 
 public class CollegeSystem {
     public static void main(String[] args) {
-        System.out.println("My name is ankita");
+       // System.out.println("My name is ankita");
         System.out.println("Hello World");
     }
 }
